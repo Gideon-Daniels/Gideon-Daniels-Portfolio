d52837077580b33d6551deab022569a62758fd75
Dragoonix <gideondaniels.dragoonix@gmail.com>
2021-05-12 16:39:39 +0200

    update

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+function showTab(tabName) {
+  // Remove active from all classes
+  let tabs = document.getElementsByClassName("main-content-sidebar");
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].classList.remove("active");
+  }
+  // Add active to specified Element
+  let selectedTab = document.getElementById(tabName);
+  selectedTab.classList.add("active");
+}
