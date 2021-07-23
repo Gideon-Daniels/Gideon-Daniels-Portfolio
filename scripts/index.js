51285597509916f9a88e9485c573fa44637822f1
Dragoonix <gideondaniels.dragoonix@gmail.com>
2021-07-23 17:45:50 +0200

    update

--- scripts/index.js
+++ scripts/index.js
@@ -9,40 +9,77 @@ function showTab(tabName) {
   selectedTab.classList.add("active");
 }
 
+
 // Project cards
 
 // 1. CREATES AN ARRAY OF OBJECTS
 
 let projects = [
   {
-    imgURL: "",
-    imgAlt: "",
-    title:  "",
-    techStack: "",
-    description: "",
-    githubURL: "#",
-    liveProjectURL: "#"
-  }
+    imgURL: "/images/Screenshot from 2021-05-04 12-00-59.png",
+    imgAlt: "Contact Form",
+    title:  "Contact Form",
+    techStack: "HTML/CSS",
+    description: "A basic contact form that allows the user to contact me for any querries .",
+    githubURL: "https://hopeful-yalow-dab42a.netlify.app/",
+    liveProjectURL: "https://github.com/Gideon-Daniels/Contact_Form.git"
+  },
+  {
+    imgURL: "/images/Screenshot from 2021-05-04 11-59-14.png",
+    imgAlt: "Timeline",
+    title:  "Timeline",
+    techStack: "HTML/CSS",
+    description: "A basic contact form that allows the user to contact me for any querries .",
+    githubURL: "https://vibrant-spence-b6ee75.netlify.app/",
+    liveProjectURL: "https://github.com/Gideon-Daniels/Timeline.git"
+  },
+  {
+    imgURL: "/images/Screenshot from 2021-05-14 10-57-09.png",
+    imgAlt: "Project Cards",
+    title:  "Project Cards",
+    techStack: "HTML/CSS",
+    description: "A basic contact form that allows the user to contact me for any querries .",
+    githubURL: "https://github.com/Gideon-Daniels/Project-Cards.git",
+    liveProjectURL: "https://optimistic-kare-819acc.netlify.app/"
+  },
+  {
+    imgURL: "/images/Screenshot from 2021-05-04 11-55-48.png",
+    imgAlt: "Testimonials",
+    title:  "Testimonials",
+    techStack: "HTML/CSS",
+    description: "A basic contact form that allows the user to contact me for any querries .",
+    githubURL: "https://github.com/Gideon-Daniels/Testimonial-Slide.git",
+    liveProjectURL: "https://dreamy-kirch-045081.netlify.app/"
+  },
+  {
+    imgURL: "../images/Screenshot from 2021-05-04 12-00-59.png",
+    imgAlt: "Contact Form",
+    title:  "Contact Form",
+    techStack: "Python",
+    description: "A basic contact form that allows the user to contact me for any querries .",
+    githubURL: "https://hopeful-yalow-dab42a.netlify.app/",
+    liveProjectURL: "https://github.com/Gideon-Daniels/Contact_Form.git"
+  },
 ]
 
 // 2. Creates the card based on the properties set from (1) (factory function)
 function createdCard(card){
   let createdCard = `
-  <div class="project-card techStack=${card.techStack}">
+  <div class="project-card" techStack=${card.techStack}>
                 <div class="card-image">
-                  <img src=${card.imgURL}alt=${card.imgAlt}/>
+                  <img src="${card.imgURL}" alt="${card.imgAlt}"/>
                 </div>
                 <div class="card-container">
-                  <h2 class="heading">${card.title}</h2>
+                  <h2 class="heading"> ${card.title}</h2>
                   <div class="card-details">
                     <p class="card-description">${card.description}</p>
                   </div>
                   <div class="buttons">
                     <button class="button live">
-                      <a href=${card.liveProjectURL}>Live</a>
+                      <a href="${card.liveProjectURL}">Live</a>
                     </button>
                     <button class="button github">
-                      <a href=${card.githubURL}>Github</a>
+                      <a href="${card.githubURL}">Github</a>
                     </button>
                   </div>
                 </div>           
@@ -53,7 +90,7 @@ function createdCard(card){
 
 // 3. Loops through all cards and displays it on screen
 function renderCards(){
-  let projectContainer = document.querySelector(".project-container");
+  let projectContainer = document.querySelector(".project-cards-container");
   for(project of projects){
     let card = createdCard(project);
     projectContainer.innerHTML += card;
@@ -68,14 +105,14 @@ function filterCards(category){
 
   for(card in cards){
     console.log(card);
-    card.style.display = "none";
+    // card.style.display = "none";
   }
 
-  let selectedCards = document.querySelectorAll(`[techStack='${category}']`);
+  // let selectedCards = document.querySelectorAll(`[techStack='${category}']`);
 
-  for (card of selectedCards){
-    card.style.display = "block"
-  }
+  // for (card of selectedCards){
+  //   card.style.display = "none"
+  // }
 }
 
 
