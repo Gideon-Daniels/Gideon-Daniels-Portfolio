29f27108111cadd309858601a66d163cf9c4e6b7
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-11-25 16:13:29 +0200

    added color themes

---
+++ scripts/colorChange.js
@@ -0,0 +1,39 @@
+function themeChange(theme) {
+  let property = document.documentElement.style;
+
+  let color = "";
+  if (theme == "first-theme") {
+    backgroundGradient =
+      "linear-gradient(to bottom right, #50a3a2 0%, #78cc6d 100%)";
+    containerBackground = "rgba(255, 255, 255, 0.2)";
+    primaryFontColor = "#323232";
+    secondaryFontColor = "#50a3a2";
+    highlightedFontColor = "#78cc6d";
+    borderMainContentSidebar = "1px solid rgba(255, 255, 255, 0.18)";
+  } else if (theme == "second-theme") {
+    backgroundGradient =
+      "linear-gradient(to bottom right, #50a3a2 0%, #323232 100%)";
+    containerBackground = "rgba(55, 155, 155, 0.2)";
+    primaryFontColor = "#323232";
+    secondaryFontColor = "#50a3a2";
+    highlightedFontColor = "#7dcc6d";
+    borderMainContentSidebar = "1px solid rgba(255, 255, 255, 0.18)";
+  } else if (theme == "third-theme") {
+    backgroundGradient =
+      "white";
+    containerBackground = "rgba(45, 45, 45, 0.2)";
+    primaryFontColor = "#323232";
+    secondaryFontColor = "#50a3a2";
+    highlightedFontColor = "#7dcc6d";
+    borderMainContentSidebar = "1px solid rgba(255, 255, 255, 0.18)";
+  }
+  property.setProperty("--background-gradient", backgroundGradient);
+  property.setProperty("--container-background", containerBackground);
+  property.setProperty("--primary-font-color", primaryFontColor);
+  property.setProperty("--secondary-font-color", secondaryFontColor);
+  property.setProperty(" --highlighted-font-color", highlightedFontColor);
+  property.setProperty(
+    "--border-main-content-sidebar",
+    borderMainContentSidebar
+  );
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,13 @@
-
 // navigation between each section
-function showTab(tabName) {
+function showTab(tabName, i) {
   // Remove active from all classes
   let tabs = document.getElementsByClassName("main-content-sidebar");
   for (let i = 0; i < tabs.length; i++) {
     tabs[i].classList.remove("active");
   }
+
   // Add active to specified Element
   let selectedTab = document.getElementById(tabName);
   selectedTab.classList.add("active");
+
 }
-
-
-
-
-
-
-
-
